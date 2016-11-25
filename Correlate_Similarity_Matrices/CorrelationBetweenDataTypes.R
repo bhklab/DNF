8140045eafe4a24b1602b8eb3fe5739408cfb9f1
DGendoo <deena.gendoo1984@gmail.com>
2016-11-25 14:15:41 -0500

    Cleanup

--- Correlate_Similarity_Matrices/CorrelationBetweenDataTypes.R
+++ Correlate_Similarity_Matrices/CorrelationBetweenDataTypes.R
@@ -5,8 +5,8 @@
 ################################################################################################
 ################################################################################################
 
-bp<-matrix(ncol = 1,nrow = 3)
-bp[1]<-bp[2]<-bp[3]<-0.15
+bp<-matrix(ncol = 1,nrow = 6)
+bp[1]<-bp[2]<-bp[3]<-bp[4]<-bp[5]<-bp[6]<-0.15
 
 #CTRPv2-based DNF
 ################################
@@ -16,19 +16,32 @@ load("strcSimilarityMat-ctrpv2.RData")
 load("sensSimilarityMat-ctrpv2.RData")
 #load Perturbation Affinity Matrix
 load("pertSimilarityMat-ctrpv2.RData")
+#load DNF itself!
+load("integrationSimilarityMat-ctrpv2.RData")
 
 #compute correlations between pairs of data types
+#Single Layers against themselves
 # Structure-Sensitivity
 Struct.Vs.Sens<-cor.test(strcAffMat,sensAffMat,method = "spearman",alternative = "two.sided")
 # Structure - Perturbation
 Struct.Vs.Pert<-cor.test(strcAffMat,pertAffMat,method = "spearman",alternative = "two.sided")
 # Perturbation - Sensitivity
 Pert.Vs.Sens<-cor.test(pertAffMat,sensAffMat,method = "spearman",alternative = "two.sided")
+# Single layers against DNF
+# DNF- Pert
+DNF.Vs.Pert<-cor.test(integrtStrctSensPert,pertAffMat,method = "spearman",alternative = "two.sided")
+# DNF - Sens
+DNF.Vs.Sens<-cor.test(integrtStrctSensPert,sensAffMat,method = "spearman",alternative = "two.sided")
+# DNF - Struct
+DNF.Vs.Struct<-cor.test(integrtStrctSensPert,strcAffMat,method = "spearman",alternative = "two.sided")
 
-CTRPv2_Cors<-c(Struct.Vs.Pert$estimate,Struct.Vs.Sens$estimate,Pert.Vs.Sens$estimate)
-names(CTRPv2_Cors)<-c("Struct vs Pert","Struct vs Sens","Pert vs Sens")
+CTRPv2_Cors<-c(Struct.Vs.Pert$estimate,Struct.Vs.Sens$estimate,Pert.Vs.Sens$estimate,NA,
+               DNF.Vs.Pert$estimate,DNF.Vs.Sens$estimate,DNF.Vs.Struct$estimate)
+names(CTRPv2_Cors)<-c("Struct vs Pert","Struct vs Sens","Pert vs Sens","", 
+                      "DNF vs Pert","DNF vs Sens", "DNF vs Struct")
 
-CTRPv2_PVals<-c(prettyNum(Struct.Vs.Pert$p.value,digits=3),prettyNum(Struct.Vs.Sens$p.value,digits=3),prettyNum(Pert.Vs.Sens$p.value,digits=3))
+CTRPv2_PVals<-c(prettyNum(Struct.Vs.Pert$p.value,digits=3),prettyNum(Struct.Vs.Sens$p.value,digits=3),prettyNum(Pert.Vs.Sens$p.value,digits=3),
+                prettyNum(DNF.Vs.Pert$p.value,digits=3),prettyNum(DNF.Vs.Sens$p.value,digits=3),prettyNum(DNF.Vs.Struct$p.value,digits=3))
 
 barplot(CTRPv2_Cors,col = "#d9ef8b",las=2,ylim=c(0,1))
 text(bp,labels = CTRPv2_PVals)
@@ -42,19 +55,34 @@ load("strcSimilarityMat-nci60.RData")
 load("sensSimilarityMat-nci60.RData")
 #load Perturbation Affinity Matrix
 load("pertSimilarityMat-nci60.RData")
+#load DNF itself!
+load("integrationSimilarityMat-nci60.RData")
 
 #compute correlations between pairs of data types
+#Single Layers against themselves
 # Structure-Sensitivity
 Struct.Vs.Sens<-cor.test(strcAffMat,sensAffMat,method = "spearman",alternative = "two.sided")
 # Structure - Perturbation
 Struct.Vs.Pert<-cor.test(strcAffMat,pertAffMat,method = "spearman",alternative = "two.sided")
 # Perturbation - Sensitivity
 Pert.Vs.Sens<-cor.test(pertAffMat,sensAffMat,method = "spearman",alternative = "two.sided")
+# Single layers against DNF
+# DNF- Pert
+DNF.Vs.Pert<-cor.test(integrtStrctSensPert,pertAffMat,method = "spearman",alternative = "two.sided")
+# DNF - Sens
+DNF.Vs.Sens<-cor.test(integrtStrctSensPert,sensAffMat,method = "spearman",alternative = "two.sided")
+# DNF - Struct
+DNF.Vs.Struct<-cor.test(integrtStrctSensPert,strcAffMat,method = "spearman",alternative = "two.sided")
 
-NCI60_Cors<-c(Struct.Vs.Pert$estimate,Struct.Vs.Sens$estimate,Pert.Vs.Sens$estimate)
-names(NCI60_Cors)<-c("Struct vs Pert","Struct vs Sens","Pert vs Sens")
 
-NCI60_PVals<-c(prettyNum(Struct.Vs.Pert$p.value,digits=3),prettyNum(Struct.Vs.Sens$p.value,digits=3),prettyNum(Pert.Vs.Sens$p.value,digits=3))
+
+NCI60_Cors<-c(Struct.Vs.Pert$estimate,Struct.Vs.Sens$estimate,Pert.Vs.Sens$estimate,NA,
+              DNF.Vs.Pert$estimate,DNF.Vs.Sens$estimate,DNF.Vs.Struct$estimate)
+names(NCI60_Cors)<-c("Struct vs Pert","Struct vs Sens","Pert vs Sens","", 
+                     "DNF vs Pert","DNF vs Sens", "DNF vs Struct")
+
+NCI60_PVals<-c(prettyNum(Struct.Vs.Pert$p.value,digits=3),prettyNum(Struct.Vs.Sens$p.value,digits=3),prettyNum(Pert.Vs.Sens$p.value,digits=3),
+               prettyNum(DNF.Vs.Pert$p.value,digits=3),prettyNum(DNF.Vs.Sens$p.value,digits=3),prettyNum(DNF.Vs.Struct$p.value,digits=3))
 
 barplot(NCI60_Cors,col = "#fee08b",las=2,ylim=c(0,1))
 text(bp,labels = NCI60_PVals)
@@ -64,9 +92,9 @@ text(bp,labels = NCI60_PVals)
 pdf("CorrelationBetweenSingleLayers.pdf",width = 10)
 par(mfrow=c(1,2),mai=c(1.82,0.82,0.82,0.82))
 barplot(CTRPv2_Cors,col = "#d9ef8b",las=2,main="CTRPv2",ylab = "Spearman Correlation",ylim=c(0,1))
-text(bp,labels = CTRPv2_PVals)
+#text(bp,labels = CTRPv2_PVals)
 barplot(NCI60_Cors,col = "#fee08b",las=2,main="NCI60",ylab = "Spearman Correlation",ylim=c(0,1))
-text(bp,labels = NCI60_PVals,las=2)
+#text(bp,labels = NCI60_PVals,las=2)
 dev.off()
 
 
