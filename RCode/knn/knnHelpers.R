64271dc6794fa1fffa4e66796740896ccfed07cb
gulara <paddze21@gmail.com>
2017-08-10 16:05:27 -0400

    Refactored knn code for modularity

--- RCode/knn/knnHelpers.R
+++ RCode/knn/knnHelpers.R
@@ -1,21 +1,30 @@
-GetKNNAccuracy <- function(k, integrated, data.bench, scale.distance=FALSE, extra.scaling=100) {
-    num.targets <- length(unique(data.bench$TARGET_NAME))
-    drug.names <- sort(unique(data.bench$MOLECULE_NAME))
-    target.names <- sort(unique(data.bench$TARGET_NAME))
+GetKNNAccuracy <- function(k, integrated, data.bench, scale.distance=FALSE, extra.scaling=100, top=5) {
+    drugs.with.targets <- sort(unique(data.bench$MOLECULE_NAME))
     
-    integrated <- integrated[drug.names, drug.names]
+    temp <- GetNearestNeighbours(k, integrated, drugs.with.targets)
     
-    counts <- matrix(data=0, nrow=length(drug.names), ncol=num.targets)
-    rownames(counts) <- drug.names
-    colnames(counts) <- sort(unique(data.bench$TARGET_NAME))
+    neighbours <- temp$neighbours
+    weights <- temp$weights
+    
+    counts <- CountTargetsFromNeighbours(neighbours, weights, data.bench, 
+                                         scale.distance, extra.scaling)
+    top.predictions <- GetTopPredictions(counts, top)
+    
+    prediction.status <- EvaluatePredictionAccuracy(top.predictions, data.bench)
+    
+    return(prediction.status)
+}
+
+GetNearestNeighbours <- function(k, integrated, drugs.with.targets) {
+    integrated <- integrated[drugs.with.targets, drugs.with.targets]
     
     # Neighbours is a a matirx of the shape NUM_DRUGS X K. 
     # For example, a row looks like: Drug X     34 21 ... 10 4.
-    neighbours <- matrix(0, nrow=length(drug.names), ncol=k)
-    rownames(neighbours) <- drug.names
+    neighbours <- matrix(0, nrow=length(drugs.with.targets), ncol=k)
+    rownames(neighbours) <- drugs.with.targets
     
-    weights <- matrix(0, nrow=length(drug.names), ncol=k)
-    rownames(weights) <- drug.names
+    weights <- matrix(0, nrow=length(drugs.with.targets), ncol=k)
+    rownames(weights) <- drugs.with.targets
     
     # For each drug, obtain the indices of the k closest neighbours
     # according to the integrated similarity measure. For some reason,
@@ -23,22 +32,42 @@ GetKNNAccuracy <- function(k, integrated, data.bench, scale.distance=FALSE, extr
     # thought that rows corresponds to points and columns correspond to 
     # dimensions).
     for (i in 1:nrow(neighbours)) {
-        row <- integrated[i, ]
-        res <- sapply(sort(row, index.return=TRUE), '[')
+        drug.name <- rownames(neighbours)[i]
+        row <- integrated[drug.name, ]
+        
+        res <- sapply(sort(row, index.return=TRUE, decreasing=T), '[')
         res.indices <- res[, "ix"]
         res.weights <- res[, 'x']
+
+        res.indices <- res.indices[2:(k+1)]
+        res.weights <- res.weights[2:(k+1)]
         
-        res.indices <- res.indices[(length(res.indices) - (k)):(length(res.indices) - 1)]
-        res.weights <- res.weights[(length(res.weights) - (k)):(length(res.weights) - 1)]
+        res.indices <- rev(res.indices)
+        res.weights <- rev(res.weights)
+
         neighbours[i, ] <- res.indices
         weights[i, ] <- res.weights
     }
     
     # Convert the indices of nearest neigbhours into the actual drug
     # names of those neighbours.
-    neighbours <- apply(neighbours, 1, function(x) {drug.names[x]})
-    neighbours <- t(neighbours)
-    rownames(neighbours) <- drug.names
+    neighbours <- apply(neighbours, 1, function(x) {drugs.with.targets[x]})
+    if (k == 1) {
+        neighbours <- as.matrix(neighbours)    
+    } else {
+        neighbours <- t(neighbours)
+        rownames(neighbours) <- drugs.with.targets        
+    }
+
+    return(list(neighbours=neighbours, weights=weights))
+}
+
+CountTargetsFromNeighbours <- function(neighbours, weights, data.bench,
+                                       scale.distance=FALSE, extra.scaling=100) {
+    num.targets <- length(unique(data.bench$TARGET_NAME))
+    counts <- matrix(data=0, nrow=nrow(neighbours), ncol=num.targets)
+    rownames(counts) <- rownames(neighbours)
+    colnames(counts) <- sort(unique(data.bench$TARGET_NAME))
     
     # Now if we want to weight the targets based on the similarities between drugs,
     # we have to determine some kind of threshold. For example, if the similarity
@@ -62,7 +91,7 @@ GetKNNAccuracy <- function(k, integrated, data.bench, scale.distance=FALSE, extr
             
             relevant.targets <- data.bench[data.bench$MOLECULE_NAME == neighbour.name, "TARGET_NAME"]
             
-            if (scale.distance == TRUE) {
+            if (scale.distance == TRUE & k > 1) {
                 counts[drug, relevant.targets] <- counts[drug, relevant.targets] + (sigmoid(weights[i, j]))    
             } else {
                 counts[drug, relevant.targets] <- counts[drug, relevant.targets] + (weights[i, j])
@@ -70,30 +99,26 @@ GetKNNAccuracy <- function(k, integrated, data.bench, scale.distance=FALSE, extr
         }
     }
     
-    counts <- apply(counts, 1, function(x) {
-        indices <- which(x > 0)
-        res <- numeric(length(x))
-        res[indices] <- exp(x[indices]) / sum(exp(x[indices]))
-        res
-    })
-    
-    counts <- t(counts)
-    colnames(counts) <- sort(unique(data.bench$TARGET_NAME))
-    
-    top.predictions <- sapply(1:nrow(counts), function(i, target.names, drug.names) {
+    return(counts)
+}
+
+GetTopPredictions <- function(counts, top=5) {
+    top.predictions <- sapply(1:nrow(counts), function(i, target.names, drugs.with.targets) {
         x <- counts[i, ]
-        sorted <- sort(x, index.return=T)
-        index.of.predictions <- tail(sorted$ix[sorted$x > 0], 5)
+        sorted <- sort(x, index.return=T, decreasing=T)
+        index.of.predictions <- head(sorted$ix[sorted$x > 0], top)
         res <- list()
-        res[[drug.names[i]]] <- x[index.of.predictions]
-        names(res[[drug.names[i]]]) <- target.names[index.of.predictions]
+        res[[drugs.with.targets[i]]] <- x[index.of.predictions]
+        names(res[[drugs.with.targets[i]]]) <- target.names[index.of.predictions]
         res
-    }, target.names=colnames(counts), drug.names=rownames(counts))
+    }, target.names=colnames(counts), drugs.with.targets=rownames(counts))
     
-    num.correct.predictions <- integer(nrow(counts))
-    names(num.correct.predictions) <- rownames(counts)
-    false.positive.targets <- integer(ncol(counts))
-    names(false.positive.targets) <- colnames(counts)
+    return(top.predictions)
+}
+
+EvaluatePredictionAccuracy <- function(top.predictions, data.bench) {
+    prediction.status <- integer(length(top.predictions))
+    names(prediction.status) <- names(top.predictions)
     
     for (i in 1:length(top.predictions)) {
         drug.name <- names(top.predictions)[i]
@@ -101,32 +126,21 @@ GetKNNAccuracy <- function(k, integrated, data.bench, scale.distance=FALSE, extr
         
         possible.true.targets <- data.bench[data.bench$MOLECULE_NAME == drug.name, "TARGET_NAME"]
         
-        # print(paste("Drug Name", drug.name, sep=": "))
-        # print(paste("Predicted Targets", target.names, sep=": "))
-        # print(paste("Possible Targets", possible.true.targets, sep=": "))
-        
         if (length(intersect(target.names, possible.true.targets)) > 0) {
-            num.correct.predictions[drug.name] <- 1
-        }
-        
-        false.positives <- setdiff(target.names, possible.true.targets)
-        if (length(false.positives) > 0) {
-            false.positive.targets[false.positives] <- false.positive.targets[false.positives] + 1
+            prediction.status[drug.name] <- 1
         }
     }
     
-    accuracy <- sum(num.correct.predictions) / length(num.correct.predictions)
-    
-    num.correct.predictions
+    return(prediction.status)
 }
 
 GetKNNAccuracySingleDrug <- function(k, integrated, data.bench, drug.name, scale.distance=FALSE, extra.scaling=100) {
     num.targets <- length(unique(data.bench$TARGET_NAME))
-    drug.names <- sort(unique(data.bench$MOLECULE_NAME))
+    drugs.with.targets <- sort(unique(data.bench$MOLECULE_NAME))
     target.names <- sort(unique(data.bench$TARGET_NAME))
     
-    counts <- matrix(data=0, nrow=length(drug.names), ncol=num.targets)
-    rownames(counts) <- drug.names
+    counts <- matrix(data=0, nrow=length(drugs.with.targets), ncol=num.targets)
+    rownames(counts) <- drugs.with.targets
     colnames(counts) <- sort(unique(data.bench$TARGET_NAME))
     
     # Neighbours is a a matirx of the shape NUM_DRUGS X K. 
@@ -154,7 +168,7 @@ GetKNNAccuracySingleDrug <- function(k, integrated, data.bench, drug.name, scale
     
     # Convert the indices of nearest neigbhours into the actual drug
     # names of those neighbours.
-    neighbours <- apply(neighbours, 1, function(x) {drug.names[x]})
+    neighbours <- apply(neighbours, 1, function(x) {drugs.with.targets[x]})
     neighbours <- t(neighbours)
     rownames(neighbours) <- drug.name
     
@@ -196,15 +210,15 @@ GetKNNAccuracySingleDrug <- function(k, integrated, data.bench, drug.name, scale
     counts <- t(counts)
     colnames(counts) <- sort(unique(data.bench$TARGET_NAME))
     
-    top.predictions <- sapply(1:nrow(counts), function(i, target.names, drug.names) {
+    top.predictions <- sapply(1:nrow(counts), function(i, target.names, drugs.with.targets) {
         x <- counts[i, ]
         sorted <- sort(x, index.return=T)
         index.of.predictions <- tail(sorted$ix[sorted$x > 0], 5)
         res <- list()
-        res[[drug.names[i]]] <- x[index.of.predictions]
-        names(res[[drug.names[i]]]) <- target.names[index.of.predictions]
+        res[[drugs.with.targets[i]]] <- x[index.of.predictions]
+        names(res[[drugs.with.targets[i]]]) <- target.names[index.of.predictions]
         res
-    }, target.names=colnames(counts), drug.names=rownames(counts))
+    }, target.names=colnames(counts), drugs.with.targets=rownames(counts))
     
     for (i in 1:length(top.predictions)) {
         drug.name <- names(top.predictions)[i]
